4c6c7fd93ab252aececae0c54261573346833798
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-07-22 22:45:04 -0300

    rota para componente home

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [MatSidenavModule, MatListModule, MatButtonModule, MatIconModule],
+  imports: [MatSidenavModule, MatListModule, MatButtonModule, MatIconModule, RouterOutlet],
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+      this.router.navigate(['home'])
+  }
 }
