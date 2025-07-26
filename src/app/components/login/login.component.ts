e1caa231d4a3cb8e05760be2555a9fba7cdd3406
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-07-26 19:53:17 -0300

    validacao forms login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,44 @@
 import {Component} from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import {FormControl, FormsModule, Validators, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import { Credenciais } from '../../models/credenciais';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
+  constructor() {
+    // Sincroniza o valor do form com creds
+    this.email.valueChanges.subscribe(value => this.creds.email = value ?? '');
+    this.senha.valueChanges.subscribe(value => this.creds.senha = value ?? '');
+  }
+
+  validaCampos(): boolean {
+    if(this.email.valid && this.senha.valid){
+      return true;
+    } else{
+      return false;
+    }
+  }
+
+  onSubmit() {
+    console.log('Dados do formulário:', this.creds);
+    // Aqui você pode chamar um serviço de autenticação, por exemplo
+    // this.authService.login(this.creds).subscribe(...)
+  }
 
 }
