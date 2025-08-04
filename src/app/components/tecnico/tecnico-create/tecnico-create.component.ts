24af552f08569eb2070be54b571135824cfb27fa
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-08-04 18:58:53 -0300

    feature: component tecnico-create criado

---
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico-create',
+  imports: [],
+  templateUrl: './tecnico-create.component.html',
+  styleUrl: './tecnico-create.component.css'
+})
+export class TecnicoCreateComponent {
+
+}
