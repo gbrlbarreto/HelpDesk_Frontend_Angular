367ea1e23c69de27e294f3484124c79f9d0bf089
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-08-05 21:46:18 -0300

    HTML e CSS para componenete TECNICO-CREATE

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, model, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule}  from '@angular/material/input';
 
 @Component({
   selector: 'app-tecnico-create',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule],
   templateUrl: './tecnico-create.component.html',
-  styleUrl: './tecnico-create.component.css'
+  styleUrls: ['./tecnico-create.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export class TecnicoCreateComponent {
-
+  readonly checked = model(false);
+  readonly indeterminate = model(false);
+  readonly labelPosition = model<'before' | 'after'>('after');
+  readonly disabled = model(false);
 }
