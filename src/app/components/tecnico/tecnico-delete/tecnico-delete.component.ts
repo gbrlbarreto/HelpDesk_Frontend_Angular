9e586a1aae9fe7ca3f3b226fee3b0bdd590f6058
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-08-10 14:47:32 -0300

    feature: componente tecnico-delete criado

---
+++ src/app/components/tecnico/tecnico-delete/tecnico-delete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico-delete',
+  imports: [],
+  templateUrl: './tecnico-delete.component.html',
+  styleUrl: './tecnico-delete.component.css'
+})
+export class TecnicoDelete {
+
+}
