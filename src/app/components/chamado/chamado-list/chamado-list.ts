c37ac336bacf24cd1137297749c8051bf78582b9
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-08-11 17:32:25 -0300

    feature: configurando rota para chamado-list

--- src/app/components/chamado/chamado-list/chamado-list.ts
+++ src/app/components/chamado/chamado-list/chamado-list.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-chamado-list',
+  standalone: true,
   imports: [],
-  templateUrl: './chamado-list.html',
-  styleUrl: './chamado-list.css'
+  templateUrl: './chamado-list.component.html',
+  styleUrls: ['./chamado-list.component.css']
 })
-export class ChamadoList {
+export class ChamadoListComponent {
 
 }
