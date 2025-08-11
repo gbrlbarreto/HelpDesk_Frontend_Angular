a8b348bb09ba875dd37adb8dde987be67f8aeeff
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-08-11 17:23:31 -0300

    feature: componente chamado-list criado

---
+++ src/app/components/chamado/chamado-list/chamado-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chamado-list',
+  imports: [],
+  templateUrl: './chamado-list.html',
+  styleUrl: './chamado-list.css'
+})
+export class ChamadoList {
+
+}
