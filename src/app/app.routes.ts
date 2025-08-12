b7be3256381450bf1def947910bd370e09fa2928
gbrlbarreto <gbrlbarreto72@hotmail.com>
2025-08-12 12:20:22 -0300

    feature: filtro para chamados implementados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
-import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list';
+import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
---
+++ src/app/components/chamado/chamado-list/chamado-list.component.ts
@@ -0,0 +1,93 @@
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { RouterLink, RouterModule } from '@angular/router';
+import { Chamado } from '../../../models/chamado';
+import { ChamadoService } from '../../../services/chamado.service';
+import { MatRadioModule } from '@angular/material/radio';
+
+@Component({
+  selector: 'app-chamado-list',
+  standalone: true,
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatPaginatorModule, MatRadioModule, MatTableModule, RouterModule, RouterLink],
+  templateUrl: './chamado-list.component.html',
+  styleUrls: ['./chamado-list.component.css']
+})
+export class ChamadoListComponent implements AfterViewInit {
+
+  ELEMENT_DATA: Chamado[] = [];
+  FILTERED_DATA: Chamado[] = [];
+
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'tecnico', 'dataAbertura', 'status', 'prioridade', 'acoes'];
+  dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private service: ChamadoService) {}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  findAll(){
+    this.service.findAll().subscribe(resposta => {
+      this.ELEMENT_DATA = resposta;
+      this.dataSource = new MatTableDataSource<Chamado>(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  retornaStatus(status: any): string {
+    if(status == '0') {
+      return 'ABERTO';
+    } else if (status == '1') {
+      return 'EM ANDAMENTO';
+    } else {
+      return 'ENCERRADO';
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade == '0') {
+      return 'BAIXA';
+    } else if (prioridade == '1') {
+      return 'MÉDIA';
+    } else {
+      return 'ALTA';
+    }
+  }
+
+  orderByStatus(status: any): void {
+    let list_status: Chamado[] = [];
+    this.ELEMENT_DATA.forEach(element => {
+      if(element.status == status)
+        list_status.push(element)
+    });
+    this.FILTERED_DATA = list_status;
+    this.dataSource = new MatTableDataSource<Chamado>(this.FILTERED_DATA);
+    this.dataSource.paginator = this.paginator;
+  }
+
+    orderByPrioridade(prioridade: any): void {
+    let list_prioridade: Chamado[] = [];
+    this.ELEMENT_DATA.forEach(element => {
+      if(element.prioridade == prioridade)
+        list_prioridade.push(element)
+    });
+    this.FILTERED_DATA = list_prioridade;
+    this.dataSource = new MatTableDataSource<Chamado>(this.FILTERED_DATA);
+    this.dataSource.paginator = this.paginator;
+  }
+}
